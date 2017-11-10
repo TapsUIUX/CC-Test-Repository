3c8281125c9e4148a5036bf59b9b99351b44a5ce
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2017-11-10 12:47:40 -0500

    added logic for foratted data

--- AngComponentsJS/controller.js
+++ AngComponentsJS/controller.js
@@ -13,6 +13,38 @@ angular.module('myApp')
         dataService.getData().then(function (response) {
             
             $scope.dataObj = response;
+            
+            
+              response.forEach((el, id, arr) => {
+                $scope.dataObjVal.push(Object.values(arr[id]));
+                  //creating unique array
+                if (!$scope.dataObjKey.some((el4, id4, arr4) => (el4 == Object.values(arr[id])[0]))) {
+                    $scope.dataObjKey.push([Object.values(arr[id])[0]]);
+                }
+            })
+            $scope.dataObjKey.sort();
+            //putting space for proper format
+            $scope.dataObjKey.forEach((el5, id5, arr5) => {
+                $scope.dataFormated.push([el5[0], '-', '-', '-', '-', '-', '-']);
+            })
+
+            //$log.info("before:" ,$scope.dataObjVal,$scope.dataObjKey,$scope.dataFormated)
+            //Creating Required Array.
+            $scope.dataObjKey.forEach((el2, id2, arr2) => {
+                $scope.dataObjVal.forEach((el3, id3, arr3) => {
+                    if (el2[0] == el3[0]) {
+                        if (el3[1] == 'C1') {
+                            $scope.dataFormated[id2].splice(1, 2, el3[1], el3[2]);
+                        }
+                        if (el3[1] == 'C2') {
+                            $scope.dataFormated[id2].splice(3, 2, el3[1], el3[2]);
+                        }
+                        if (el3[1] == 'C3') {
+                            $scope.dataFormated[id2].splice(5, 2, el3[1], el3[2]);
+                        }
+                    }
+                })
+            })
 
         }, function (rejected) {
             $log.info(rejected)
@@ -20,8 +52,7 @@ angular.module('myApp')
 
 
         $scope.sortColumn = function (column) {
-            //if cleicked first time it will set the order to false means ASC
-            //if not it will change the order.
+            //Settig sort order on click.
             $scope.sortOrder = ($scope.sortColumnName == column) ? !$scope.sortOrder : false;
             $scope.sortColumnName = column;
         }
